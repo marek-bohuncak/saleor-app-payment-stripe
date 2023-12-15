46be6f9aae3c1259050eb864bbd880de958c11ea
Marek Bohuncak <marek.b@certua.io>
2023-12-15 15:47:47 +0100

    Update register.ts

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -9,17 +9,6 @@ import { saleorApp } from "../../saleor-app";
 export default createAppRegisterHandler({
   apl: saleorApp.apl,
   allowedSaleorUrls: [
-    /**
-     * You may want your app to work only for certain Saleor instances.
-     *
-     * Your app can work for every Saleor that installs it, but you can
-     * limit it here
-     *
-     * By default, every url is allowed.
-     *
-     * URL should be a full graphQL address, usually starting with https:// and ending with /graphql/
-     *
-     * Alternatively pass a function
-     */
+    "https://storeapi.holz.ai/graphql/"
   ],
 });
